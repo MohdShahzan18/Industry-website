e1bb4c8c52745d0ed1ed24def359466816acd58b
Shahzan <khanshezyy786@gmail.com>
2024-11-12 00:35:20 +0530

    Remove multiple HTML, CSS, and JS files

--- app.js
+++
@@ -1,7 +0,0 @@
-const texts = ["or Caps & Closures", "Electroplating Rectifier"];
-  let index = 0;
-
-  setInterval(() => {
-      index = (index + 1) % texts.length;
-      document.getElementById('text').textContent = texts[index];
-  }, 1500); 
